83c8d0fa9af914aaad2bbfaa11ab2432a39a40b2
Louis De Smet <louisdesmet2@hotmail.com>
2020-11-10 12:16:00 +0100

    First commit 5

--- src/App.js
+++ src/App.js
@@ -22,9 +22,6 @@ import Register from "./Register";
 
 const PrivateRoute = ({component: Component, ...rest}) => {
   return (
-
-      // Show the component only when the user is logged in
-      // Otherwise, redirect the user to /signin page
       <Route {...rest} render={props => (
           localStorage.getItem('token') ?
               <Component {...props} />
